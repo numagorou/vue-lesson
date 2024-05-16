468ec10820eba74745743b61b4377f64800d6d0f
numagorou <numagorou258259@outlook.jp>
2024-05-17 01:03:04 +0900

    reactive()とref()はこうして一緒に使えるまで

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, reactive } from 'vue'
+// let a = 1
+// let b = 2
+// let c = a + b
+// a = 4
+// console.log(c)
+const title = ref('Vue.js Course')
+let price = ref(9.99)
+console.log(price.value)
+function increment() {
+  price.value += 1
+  console.log(price)
+  instructor.age += 1
+  instructor.bio = 'hi'
+  instructor.sns.twitter = 'hello'
+  instructor.email = ref('info.example.com')
+}
+const info = ref({
+  student: 1000,
+  rating: 4
+})
+const instructor = reactive({
+  name: 'maruse',
+  age: 25,
+  sns: {
+    twitter: '@__maruse__',
+    youtube: '@maruse'
+  },
+  email: ref('maruse@example.com')
+})
+instructor.bio = 'hello'
+const items = reactive([ref(1), ref(2), ref(3)])
+items[0].value
+console.log(instructor.email)
+console.log(instructor.age)
+console.log(info.value.student)
+console.log(instructor)
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Title:{{ title }}</h1>
+  <h2>Price: ${{ price - 1 }}</h2>
+  <button @click="increment">button</button>
+  <h2>Student:{{ info.student }}</h2>
+  <h2>Instructor age:{{ instructor.age }}</h2>
+  <h2>Instructor age:{{ instructor.bio }}</h2>
+  <h2>Instructor SNS Twitter:{{ instructor.sns.twitter }}</h2>
+  <h2>Instructor email:{{ instructor.email }}</h2>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
