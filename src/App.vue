aea5c2ac315bcdba179c8efd014ae9c5eb3e50fa
numagorou <numagorou258259@outlook.jp>
2024-05-20 21:19:15 +0900

    watchEffectを使ってリアクティブなデータを監視する方法

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 const count = ref(0)
+watchEffect(() => {
+  console.log('watchEffect')
+  console.log(count.value)
+  setTimeout(() => {
+    console.log('after 1 second')
+  }, 1000)
+  count.value = 'hello'
+})
 </script>
 <template>
   <p>{{ count }}</p>
-  <input type="text" @keyup.space.delete="count++" />
+  <button @click="count++">+1</button>
 </template>
