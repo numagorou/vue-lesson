df57f40422959c87a456a0105fb7e009c8d578df
numagorou <numagorou258259@outlook.jp>
2024-06-06 22:21:27 +0900

    Section11終了まで

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
 import { ref } from 'vue'
-import CustomInput from '@/components/CustomInput.vue'
-const userInput = ref('hello')
-const titleInput = ref('title')
+import { useRefLimitedHistory } from './composables/reLimitedHistory'
+
+const count = ref(0)
+
+const { history, undo } = useRefLimitedHistory(count, ref(3))
+const userInput = ref('')
+const { history: history2, undo: undo2 } = useRefLimitedHistory(userInput, 3)
 </script>
 <template>
-  <h1>v-model</h1>
-  <CustomInput v-model.uppercase="userInput" v-model:title-name="titleInput" />
-  <p>userInput:{{ userInput }}</p>
-  <p>titleInput:{{ titleInput }}</p>
+  <p>count: {{ count }}</p>
+  <button @click="count++">+1</button>
+  <p>history: {{ history }}</p>
+  <button @click="undo">undo</button>
+  <p>count: {{ count }}</p>
+  <input v-model="userInput" type="text" />
+  <p>history2: {{ history2 }}</p>
+  <button @click="undo2">undo2</button>
 </template>
---
+++ src/composables/reLimitedHistory.js
@@ -0,0 +1,14 @@
+import { watchEffect, toValue} from 'vue'
+import { useRefHistory } from './refHistory'
+export function useRefLimitedHistory(source, capacity) {
+  const { history, undo } = useRefHistory(source)
+  watchEffect(() => {
+    if (history.value.length > toValue(capacity)) {
+      history.value.shift()
+    }
+  })
+  return {
+    history,
+    undo
+  }
+}
