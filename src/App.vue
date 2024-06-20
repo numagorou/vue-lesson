6f217db60d9c16c817063824cf9308599ef15f79
numagorou <numagorou258259@outlook.jp>
2024-06-21 00:32:36 +0900

    section13 127まで

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,8 @@
-<script setup>
-import { ref, reactive } from 'vue'
-// let a = 1
-// let b = 2
-// let c = a + b
-// a = 4
-// console.log(c)
-const title = ref('Vue.js Course')
-let price = ref(9.99)
-console.log(price.value)
-function increment() {
-  price.value += 1
-  console.log(price)
-  instructor.age += 1
-  instructor.bio = 'hi'
-  instructor.sns.twitter = 'hello'
-  instructor.email = ref('info.example.com')
-}
-const info = ref({
-  student: 1000,
-  rating: 4
-})
-const instructor = reactive({
-  name: 'maruse',
-  age: 25,
-  sns: {
-    twitter: '@__maruse__',
-    youtube: '@maruse'
-  },
-  email: ref('maruse@example.com')
-})
-instructor.bio = 'hello'
-const items = reactive([ref(1), ref(2), ref(3)])
-items[0].value
-console.log(instructor.email)
-console.log(instructor.age)
-console.log(info.value.student)
-console.log(instructor)
-const { sections } = {
-  sections: ref(10),
-  language: ref('Japanese')
-}
-</script>
+<script setup></script>
 <template>
-  <h1>Title:{{ title }}</h1>
-  <h2>Price: ${{ price - 1 }}</h2>
-  <button @click="increment">button</button>
-  <h2>Student:{{ info.student }}</h2>
-  <h2>Instructor age:{{ instructor.age }}</h2>
-  <h2>Instructor age:{{ instructor.bio }}</h2>
-  <h2>Instructor SNS Twitter:{{ instructor.sns.twitter }}</h2>
-  <h2>Instructor email:{{ instructor.email }}</h2>
-  <h2>Course Info Sections:{{ sections }}</h2>
+  <h1>Vue Router</h1>
+  <RouterLink :to="'/'">Home</RouterLink> |
+  <RouterLink :to="{ path: '/about', query: { lang: 'ja' }, hash: '#title' }">About</RouterLink>
+  <a href="https://vuejs.org">vue</a>
+  <RouterView />
 </template>
