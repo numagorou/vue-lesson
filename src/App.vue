ce8bf900ddcac18dc43ea0402384af28c69ec77f
numagorou <numagorou258259@outlook.jp>
2024-06-28 01:43:13 +0900

    section13 130パラメータを(:)使って動的なパスを作る方法

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <script setup></script>
 <template>
   <h1>Vue Router</h1>
-  <RouterLink :to="'/'">Home</RouterLink> |
-  <RouterLink :to="{ path: '/about', query: { lang: 'ja' }, hash: '#title' }">About</RouterLink>
-  <a href="https://vuejs.org">vue</a>
+  <RouterLink :to="{ name: 'home' }">Home</RouterLink> |
+  <RouterLink :to="{ name: 'about' }">About</RouterLink>
   <RouterView />
 </template>
